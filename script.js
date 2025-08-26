0b4a0d27bf0a94aa23da768e703c7b0240fd1ebd
joanjacquin <201502077+joanjacquin@users.noreply.github.com>
2025-08-26 17:28:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+setInterval(function() {
+                const link = document.querySelector('div.centered_box button');
+                if (link) {
+                    link.click();
+                }
+            }, 200); //in milliseconds

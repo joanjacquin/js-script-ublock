2f87de2a03ce96b3cadf86375ad56f51eca3c2b4
joanjacquin <201502077+joanjacquin@users.noreply.github.com>
2025-08-26 17:54:20 +0200

    Update script.js
    added songsterr block

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-///deezer.js
+/// deezer.js
 setInterval(function() {
                 const link = document.querySelector('div.centered_box button');
+                if (link) {
+                    link.click();
+                }
+            }, 200); //in milliseconds
+
+
+/// songsterr.js
+setInterval(function() {
+                const link = document.querySelector('.rq1ph a');
                 if (link) {
                     link.click();
                 }
